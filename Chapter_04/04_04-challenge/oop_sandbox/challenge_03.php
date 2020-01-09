24b2c3e6f918a1ed58a48f38b4edbf683f6ef4e0
Nick Ferrari <njferrari14@gmail.com>
2020-01-09 09:37:11 -0600

    added instances and tests to ch4 challenge for visibility

--- Chapter_04/04_04-challenge/oop_sandbox/challenge_03.php
+++ Chapter_04/04_04-challenge/oop_sandbox/challenge_03.php
@@ -42,7 +42,32 @@ class Unicycle extends Bicycle {
 
 }
 
+$trek = new Bicycle;
+$trek->brand = 'Trek';
+$trek->model = 'Emonda';
+$trek->year = '2017';
 
+$uni = new Unicycle;
 
+echo "Bicycle: " . $trek->wheel_details() . "<br />";
+echo "Unicycle: " . $uni->wheel_details() . "<br />";
+echo "<hr />";
+
+echo "Set weight using kg<br />";
+$trek->set_weight_kg(1);
+echo $trek->weight_kg() . "<br />";
+echo $trek->weight_lbs() . "<br />";
+echo "<hr />";
+
+echo "Set weight using lbs<br />";
+$trek->set_weight_lbs(2);
+echo $trek->weight_kg() . "<br />";
+echo $trek->weight_lbs() . "<br />";
+echo "<hr />";
+
+echo "Set weight for Unicycle<br />";
+$uni->set_weight_kg(1);
+echo $uni->weight_kg() . "<br />";
+echo $uni->weight_lbs() . "<br />";
 
 ?>
