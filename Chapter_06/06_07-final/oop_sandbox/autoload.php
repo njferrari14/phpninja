ec30ba416ccfc153f42eda60dff557e1137f5c3d
Nick Ferrari <njferrari14@gmail.com>
2020-01-14 08:19:53 -0600

    finished chapter 6

--- Chapter_06/06_07-final/oop_sandbox/autoload.php
+++ Chapter_06/06_07-final/oop_sandbox/autoload.php
@@ -1,12 +1,11 @@
 <?php
 
 function my_autoload($class) {
-  if(preg_match('/\A\w+\Z/', $class)) {
-    include 'classes/' . $class . '.class.php';
-  }
+	if(preg_match('/\A\w+\Z/', $class)) {
+		include 'classes/' . $class . '.class.php';
+	}
 }
 spl_autoload_register('my_autoload');
-
 
 $bike = new Unicycle;
 $bike->brand = 'Trek';
